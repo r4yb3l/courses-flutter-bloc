c492905aadc798e40b766664228f1d04f477059e
r4yb3l <108087778+r4yb3l@users.noreply.github.com>
2024-05-20 14:57:42 -0600

    3. Implemented MultiBloc Provider in main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:blocs_app/config/config.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(MyBlocProvider());
+  // runApp(const MyApp());
+}
 
+class MyBlocProvider extends StatelessWidget {
+  const MyBlocProvider({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(providers: [], child: const MyApp());
+  }
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp.router(
       title: 'Flutter BLoC',
       debugShowCheckedModeBanner: false,
       routerConfig: appRouter,
-      theme: AppTheme( isDarkmode: false ).getTheme(),
+      theme: AppTheme(isDarkmode: false).getTheme(),
     );
   }
 }
