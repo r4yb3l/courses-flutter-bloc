e722e85d8f3ca37fd158f8ff3c158ea965d186ee
r4yb3l <108087778+r4yb3l@users.noreply.github.com>
2024-05-21 09:07:07 -0600

    4. Implemented guests events

--- lib/presentation/blocs/04-guests/guests_event.dart
+++ lib/presentation/blocs/04-guests/guests_event.dart
@@ -3,3 +3,27 @@ part of 'guests_bloc.dart';
 sealed class GuestsEvent extends Equatable {
   const GuestsEvent();
 }
+
+final class SetAllFilterEvent extends GuestsEvent {
+  @override
+  List<Object?> get props => [];
+}
+
+final class SetInvitedFilterEvent extends GuestsEvent {
+  @override
+  List<Object?> get props => [];
+}
+
+final class SetNotInvitedFilterEvent extends GuestsEvent {
+  @override
+  List<Object?> get props => [];
+}
+
+final class SetCustomFilterEvent extends GuestsEvent {
+  final GuestFilter guestFilter;
+
+  const SetCustomFilterEvent({required this.guestFilter});
+
+  @override
+  List<Object?> get props => [];
+}
