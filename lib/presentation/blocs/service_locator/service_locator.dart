900997aae325b04c5b8317e6e7bcc45460a1d975
r4yb3l <108087778+r4yb3l@users.noreply.github.com>
2024-05-21 08:35:14 -0600

    1. Created Service locator and registered Cubits as Singletons

---
+++ lib/presentation/blocs/service_locator/service_locator.dart
@@ -0,0 +1,12 @@
+import 'package:blocs_app/config/config.dart';
+import 'package:blocs_app/presentation/blocs/blocs.dart';
+import 'package:get_it/get_it.dart';
+
+GetIt getIt = GetIt.instance;
+
+void ServiceLocatorInit() {
+  getIt.registerSingleton(UsernameCubit());
+  getIt.registerSingleton(RouterSimpleCubit());
+  getIt.registerSingleton(CounterCubit());
+  getIt.registerSingleton(ThemeCubit());
+}
